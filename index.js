82a877f378f3f26f75b2cdc8ec03864799bcb9eb
Derrick Nyasio <dnyasio2014@gmail.com>
2022-08-04 15:39:39 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,43 @@
+const add = (a, b) => {
+    return a + b;
+}
+
+const subtract = (a, b) => {
+    return a - b;
+}
+
+const multiply = (a, b) => {
+    return a * b;
+}
+
+const divide = (a, b) => {
+    return a / b;
+}
+
+const increment = (a) => {
+    return a + 1;
+}
+
+const decrement = (a) => {
+    return a - 1;
+}
+
+function makeInt(a) {
+    return parseInt(a);
+}
+
+function makeInt(a) {
+    return parseInt(a, 10);
+}
+
+const base = (a) => {
+    return parseInt(a, 2);
+}
+
+const makeFloat = (a) => {
+    return parseFloat(a);
+}
+
+const preserveDecimal = (a) => {
+    return parseFloat(a);
+}
